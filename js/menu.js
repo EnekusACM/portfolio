3b5157ee9b439d1cf4ba08eeb530d5e33f49db12
Enekus <122829721+EnekusACM@users.noreply.github.com>
2025-04-26 19:33:30 +0200

    menu lateral

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleMenu').addEventListener('click', () => {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('open'); // Alternar la clase 'open' en el contenedor #sidebar
+});
