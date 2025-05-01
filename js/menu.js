7b6fa83319df84354dfb3a45ff76e94fe75daee8
Enekus <122829721+EnekusACM@users.noreply.github.com>
2025-05-01 10:16:09 +0200

    actualizacion arreglos

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,12 @@
-document.getElementById('toggleMenu').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleMenuButton = document.getElementById('toggleMenu');
     const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('open'); // Alternar la clase 'open' en el contenedor #sidebar
+
+    if (toggleMenuButton && sidebar) {
+        toggleMenuButton.addEventListener('click', () => {
+            sidebar.classList.toggle('open'); // Alternar la clase 'open' en el contenedor #sidebar
+        });
+    } else {
+        console.error('No se encontraron los elementos #toggleMenu o #sidebar en el DOM.');
+    }
 });
