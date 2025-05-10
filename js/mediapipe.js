096964a72bdc922e02c118cf9b5f4234fb7434aa
Enekus <122829721+EnekusACM@users.noreply.github.com>
2025-05-10 10:09:27 +0200

    cambio deteccion

--- js/mediapipe.js
+++ js/mediapipe.js
@@ -1,6 +1,7 @@
 const videoElement = document.getElementById('camera');
 const canvasElement = document.getElementById('output');
 const canvasCtx = canvasElement.getContext('2d');
+
 // Configurar MediaPipe Hands
 const hands = new Hands({
   locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
@@ -11,24 +12,37 @@ hands.setOptions({
   minDetectionConfidence: 0.5,
   minTrackingConfidence: 0.5,
 });
+
 hands.onResults((results) => {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-    results.image, 0, 0, canvasElement.width, canvasElement.height
-  );
+
+  // Dibujar la máscara de la mano detectada
+  if (results.segmentationMask) {
+    canvasCtx.drawImage(
+      results.segmentationMask,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+  }
+
+  // Opcional: Dibujar los puntos de referencia de la mano
   if (results.multiHandLandmarks) {
     for (const landmarks of results.multiHandLandmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {color: '#00FF00', lineWidth: 5});
-      drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#00FF00', lineWidth: 5 });
+      drawLandmarks(canvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
     }
   }
+
   canvasCtx.restore();
 });
+
 // Configurar cámara
 const camera = new Camera(videoElement, {
   onFrame: async () => {
-    await hands.send({image: videoElement});
+    await hands.send({ image: videoElement });
   },
   width: 480,
   height: 240,
